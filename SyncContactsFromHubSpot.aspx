74ffb612a25f50ae0b02748e1fe1a3bc65b8272a
NiharikaXtendLabs <121125272+NiharikaXtendLabs@users.noreply.github.com>
2022-12-21 17:34:16 +0530

    Add files via upload
    Initial commit

---
+++ SyncContactsFromHubSpot.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" Async="true"
+    CodeBehind="SyncContactsFromHubSpot.aspx.cs" Inherits="HubspotContactsToSqlServer.SyncContactsFromHubSpot" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>HubSpot Contacts</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:DataGrid ID="HubSpotContactsDataGrid" runat="server">
+        </asp:DataGrid>
+    </form>
+</body>
+</html>
---
+++ SyncContactsFromHubSpot.aspx.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Data;
+using HubspotContactsToSqlServer.Helper;
+
+namespace HubspotContactsToSqlServer
+{
+    public partial class SyncContactsFromHubSpot : System.Web.UI.Page
+    {
+        private static string _contactsList;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var table = new DataTable();
+            HubSpotContacts deserializedContacts = null;
+            GetContactsList();
+            if (_contactsList != null)
+            {
+                table.Columns.Add("First Name");
+                table.Columns.Add("Last Name");
+
+                deserializedContacts = JsonConvert.DeserializeObject<HubSpotContacts>(_contactsList);
+            }
+
+            //show data on web browser
+            if (deserializedContacts != null)
+            {
+                foreach (var contactsLists in deserializedContacts.Contacts)
+                {
+                    var firstName = contactsLists.Properties.FirstName.Value;
+                    var lastName = contactsLists.Properties.LastName.Value;
+                    table.Rows.Add(firstName, lastName);
+                }
+            }
+            HubSpotContactsDataGrid.DataSource = table;
+            HubSpotContactsDataGrid.DataBind();
+
+            //add data to sql server
+            SqlServerHelper.InsertDataToSqlServer(deserializedContacts);
+        }
+
+        private static async void GetContactsList()
+        {
+            _contactsList = await HubSpotApiHandler.GetContactsFromHubSpot();
+        }
+    }
+}
